0146d7a24f1e9a21b2c61cf28565d862fb3e59b4
Daniel Ratmiroff <danielratmiroff@gmail.com>
2020-08-09 13:49:14 +0200

    tippy library

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,6 +6,11 @@
         <p class="text-primary-dark mb-4 sm:ml-4 leading-tight">
             Discover more about your favorite characters
         </p>
+        <button 
+    content="I'm a Tippy tooltip!" 
+    v-tippy="{trigger : 'click'}">
+    Click
+</button>
         <Searchbar v-on:filterBySearch="searchChar" />
         <Characters v-on:openDetails="openAttributes" :charList='this.showList' />
     </div>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
 import './assets/css/tailwind.css'
+import VueTippy, { TippyComponent } from "vue-tippy";
 import ProgressBar from 'vue-simple-progress'
 import { store } from './store/store'
 import axios from "axios";
@@ -9,6 +10,7 @@ import VueAxios from "vue-axios";
 // import VueRouter from 'vue-router'
 
 Vue.use(Vuex)
+Vue.use(VueTippy)
 Vue.component('ProgressBar', ProgressBar)
 
 // Vue.use(VueRouter)
@@ -19,7 +21,7 @@ Vue.component('ProgressBar', ProgressBar)
 //   ]
 // })
 
-
+Vue.component("tippy", TippyComponent);
 new Vue({
   render: h => h(App),
   store,
