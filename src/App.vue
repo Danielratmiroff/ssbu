4b70bb2c50b046c05e38c6aa4b3e9e8a29023a2d
Daniel Ratmiroff <danielratmiroff@gmail.com>
2020-09-08 18:28:51 +0200

    new logo

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,58 @@
 <template>
-    <div id="app" class="bg-primary-white">
-        <div class="flex bg-primary-white mb-3 h-16 py-2 px-3 md:mx-4 md:my-2 
-                 mb-3 justify-between items-center">
-            <router-link :to="{ name : 'Dashboard' }" class="h-16 py-2 md:my-2"> 
-                <img src="./assets/logo.png" class="max-h-full" /> 
-            </router-link>
-            <div class="flex items-center navbar-item rounded-md border-2 border-primary-blue py-1 px-2 text-primary-blue font-bold
+  <div id="app" class="bg-primary-white">
+    <div
+      class="flex bg-primary-white mb-3 h-16 py-2 px-3 md:mx-4 md:my-2 
+                 mb-3 justify-between items-center"
+    >
+      <router-link :to="{ name: 'Dashboard' }" class="h-16 py-2 md:my-2">
+        <img src="./assets/logo.svg" class="max-h-full" />
+      </router-link>
+      <div
+        class="flex items-center navbar-item rounded-md border-2 border-primary-blue py-1 px-2 text-primary-blue font-bold
                md:text-lg
-              ">
-                About
-            </div>
-        </div>
-
-        <router-view />
-
+              "
+      >
+        About
+      </div>
     </div>
+
+    <router-view />
+  </div>
 </template>
 
 <script>
-
-export default {
-    name: 'App',
-}
+  export default {
+    name: "App",
+  };
 </script>
 
 <style lang="scss">
-
-html {
+  html {
     @apply bg-primary-white;
-}
+  }
 
-p {
+  p {
     text-rendering: optimizeLegibility;
-}
+  }
 
-.smoothScroll {
+  .smoothScroll {
     scroll-behavior: smooth;
-}
+  }
 
-#app {
+  #app {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    font-family: 'Nunito', 'Helvetica', 'Arial', sans-serif;
+    font-family: "Nunito", "Helvetica", "Arial", sans-serif;
     font-size: 16px;
-    max-width:1260px;
-    margin: 0 auto
-}
+    max-width: 1260px;
+    margin: 0 auto;
+  }
 
-.inactive {
+  .inactive {
     display: none;
-}
+  }
 
-.isActive {
+  .isActive {
     display: block;
-}
+  }
 </style>
--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,40 +1,49 @@
 <template>
-<div>
+  <div>
     <div class="full-overlay bg-contain bg-secondary-lightblue overflow-hidden">
-        <div class="relative h-full">
-            <img src="@/assets/logo.png" class="pt-6 w-16 m-auto">
-            <div class="font-bold px-4 text-center leading-tight text-primary-white" 
-                style="font-size:2.3rem;
-                ">
-                Super Smash Pros
-            </div>
-            <img src="@/assets/kirby-start.png" class="start-images kirby" />
-            <img src="@/assets/logo-start.png" class="start-images logo" />
-
-            <router-link :to="{ name : 'Dashboard' }" class="absolute bottom-0 mb-16 w-full z-50">
-                <p class="text-center w-2/4 m-auto border-primary-blue bg-primary-blue text-primary-white font-bold rounded-lg p-3 border-2 hover:bg-primary-dark cursor-pointer hover:border-primary-dark transition ease-in-out duration-300 ">
-                    Characters  
-                </p>
-            </router-link>
+      <div class="relative h-full">
+        <div class="flex m-auto justify-center pt-6 w-full m-auto items-center">
+          <img src="@/assets/logo.svg" class="w-16" />
+          <div
+            class="font-bold px-4 text-center leading-tight text-primary-white"
+            style="font-size:2.3rem;
+                "
+          >
+            Super Smash Pros
+          </div>
         </div>
+        <img src="@/assets/kirby-start.png" class="start-images kirby" />
+        <img src="@/assets/logo-start.svg" class="start-images logo" />
+
+        <router-link
+          :to="{ name: 'Dashboard' }"
+          class="absolute bottom-0 mb-16 w-full z-50"
+        >
+          <p
+            class="text-center w-2/4 m-auto border-primary-blue bg-primary-blue text-primary-white font-bold rounded-lg p-3 border-2 hover:bg-primary-dark cursor-pointer hover:border-primary-dark transition ease-in-out duration-300 "
+          >
+            Characters
+          </p>
+        </router-link>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'Start',
+  export default {
+    name: "Start",
 
     methods: {
-        openDashboard() {
-            this.$emit('loadDashboard')
-        }
-    }
-}
+      openDashboard() {
+        this.$emit("loadDashboard");
+      },
+    },
+  };
 </script>
 
 <style>
-.full-overlay {
+  .full-overlay {
     position: absolute;
     top: 0;
     height: 100%;
@@ -42,40 +51,39 @@ export default {
     left: 0;
     right: 0;
     z-index: 10;
-}
+  }
 
-.start-images {
+  .start-images {
     margin: auto;
     position: absolute;
     height: auto;
     bottom: 0;
     right: 0;
     max-width: unset;
-}
+  }
 
-.kirby {
+  .kirby {
     top: 2%;
     width: 100%;
     z-index: -1;
-}
+  }
 
-.logo {
-    left:-10%;
-    top:5%;
+  .logo {
+    left: -10%;
+    top: 5%;
     width: 115%;
     mix-blend-mode: screen;
     z-index: -2;
-}
+  }
 
-
-@media (min-width: 640px) {
+  @media (min-width: 640px) {
     .start-images {
-        max-width: 500px;
-        left: 0;
-        top: 0;
+      max-width: 500px;
+      left: 0;
+      top: 0;
     }
     .logo {
-        max-width: 800px;
+      max-width: 800px;
     }
-}
+  }
 </style>
